5ed7c1a619fbc740c5a08b8719209f6c6fde6d58
Vidya Sagar Mukkavili <vidyasagar.mukkavili@hbwsl.com>
2022-09-09 20:05:13 +0530

    Add markup and basic css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main-container">
+        <h1 className="Heading">Project: Random Quote Generator!</h1>
+
+        <div className="container__box">
+          <div className="quote-left"></div>
+          <p className="quote__paragraph">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam amet
+            velit vitae omnis deserunt
+          </p>
+          <div className="quote-author">
+            - <span id="author">Tony Robbins</span>
+          </div>
+
+          <div className="buttons">
+            <a className="button socials" id="tweet-quote" href="#"></a>
+            <a className="button socials" id="tumblr-quote" href="#"></a>
+
+            <button className="button" id="new-quote">
+              New quote
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
